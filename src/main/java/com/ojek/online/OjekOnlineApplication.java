5e60c5e58a4e0f5738fd50188c42262c29286b15
hadinuryani <wongsedah6@gmail.com>
2026-04-19 06:05:43 +0700

    application

---
+++ src/main/java/com/ojek/online/OjekOnlineApplication.java
@@ -0,0 +1,12 @@
+package com.ojek.online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjekOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OjekOnlineApplication.class, args);
+    }
+}
